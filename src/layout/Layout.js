a900869d66d12439f874c7ca6f644803bfb42ec8
HDLe <89816290+Dhle11@users.noreply.github.com>
2022-08-03 21:57:32 -0700

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,24 +4,27 @@ import NotFound from "./NotFound";
 import Menu from "./Menu";
 import { Route, Switch } from "react-router-dom";
 import Home from "../home/Home";
+import ObservationCreate from "../observations/ObservationCreate";
 
-function Layout() {
-  return (
-    <>
-      <Header />
-      <Menu />
-      <div className="container">
-        <Switch>
-          <Route exact={true} path="/">
+
+ function Layout() {
+   return (
+       <Menu />
+       <div className="container">
+         <Switch>
+          <Route path="/observations/new">
+            <ObservationCreate />
+          </Route>
+           <Route exact={true} path="/">
             <Home />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </>
-  );
-}
+           </Route>
+           <Route>
+             <NotFound />
+           </Route>
+         </Switch>
+       </div>
+     </>
+   );
+ }
 
 export default Layout;
