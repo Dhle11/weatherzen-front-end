e37a3d55957ec8b702defd8526bd65ac14383c13
HDLe <89816290+Dhle11@users.noreply.github.com>
2022-08-03 22:18:17 -0700

    Update ObservationCreate.js

--- src/observations/ObservationCreate.js
+++ src/observations/ObservationCreate.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createObservation } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function ObservationCreate() {
   const history = useHistory();
-  
+  const [error, setError] = useState(null);
   const [observation, setObservation] = useState({
     latitude: "",
     longitude: "",
@@ -17,9 +18,11 @@ function ObservationCreate() {
 
   function submitHandler(event) {
     event.preventDefault();
-    createObservation(observation).then(() => {
-      history.push("/");
-    });
+createObservation(observation)
+      .then(() => {
+        history.push("/");
+      })
+      .catch(setError);
   }
 
   function changeHandler({ target: { name, value } }) {
@@ -29,11 +32,11 @@ function ObservationCreate() {
     }));
    }
 
-  return (
 return (
      <main>
 
       <h1 className="mb-3">Create Observation</h1>
+    <ErrorAlert error={error} />
       <form onSubmit={submitHandler} className="mb-4">
         <div className="row mb-3">
           <div className="col-6 form-group">
